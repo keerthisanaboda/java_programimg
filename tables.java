2b9e62fa295c11c82b2a7b312072e1758703f30a
keerthisanaboda <keerthisanaboda@gmail.com>
2026-07-16 23:50:48 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,14 @@
+public class Table {
+    public static void main(String[] args) {
+        int n = 10, m = 5;
+         for(int i = 1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.println(i + " * " + j  +" = " + i*j);
+            }
+            System.out.println();
+         }
+       
+
+    }
+   
+}
