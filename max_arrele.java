d4569edca71c611aacc22dbf7414f03d94502be7
keerthisanaboda <keerthisanaboda@gmail.com>
2026-07-16 23:52:38 +0530

    Create max_arrele.java
    maximum element of array

---
+++ max_arrele.java
@@ -0,0 +1,11 @@
+public class arr {
+    public static void main(String[] args) {
+       int [] a = {1,10,5,2,5,6,90,1,2,4,8} ;
+       int max =0;
+       for(int i=0;i<a.length ;i++){
+        if(a[i] > max){
+            max =  a[i];
+        }
+    } System.out.println(max);
+ }
+}
